a225dbfca554533ac288d15b69c0843271113ef9
Amit Phaltankar <amitphaltankar@Pradnyas-MBP.modem>
2024-05-31 12:29:39 +1000

    code review comment fix

--- src/main/java/com/example/test_service/TestServiceApplication.java
+++ src/main/java/com/example/test_service/TestServiceApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TestServiceApplication {
-
+	//THIS IS MY NEW CODE CHANGE
 	public static void main(String[] args) {
 		SpringApplication.run(TestServiceApplication.class, args);
 	}
